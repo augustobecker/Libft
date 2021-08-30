30fdeb8bb011efc62cf8e3c31c942217b8843029
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-08-30 12:38:18 -0300

    Update ft_calloc.c
    Return NULL if the allocation fails.

--- ft_calloc.c
+++ ft_calloc.c
@@ -4,6 +4,8 @@ void *ft_calloc(size_t nbr_elements, size_t element_size)
 	void *ptr;
 
 	ptr = malloc(nbr_elements * element_size);
+	if (ptr == NULL)
+		return (NULL);
 	ft_bzero(ptr, element_size);
 	return (ptr);
 }
