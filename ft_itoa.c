606a2009f7c7977b9974c90f81a87aa0cbeb32a8
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-08-31 06:23:34 -0300

    Update ft_itoa.c
    Update ft_rev_str to return the string so we can do this in just one line of code --> return (ft_rev_str(str)); .
    Create ft_nblength that returns the exact length of the final string.
    Update some come to fit in 25 lines.
    Add extra verifications for 0 and if malloc fails.

--- ft_itoa.c
+++ ft_itoa.c
@@ -1,47 +1,66 @@
 #include "libft.h"
-static void    ft_rev_str(char *str)
-{
-    int    i;
-    int    size;
-    int    temporary;
+static int ft_nblength(int nb);
 
-    i = 0;
-    size = ft_strlen(str) - 1;
-    while (i < size)
-    {
-        temporary = *(str + i);
-        *(str + i) = *(str + size);
-        *(str + size) = temporary;
-        i++;
-        size--;
-    }
-}
+static char *ft_rev_str(char *str);
+
 char *ft_itoa(int nb)
 {
     int     i;
-    char    *str;
     int     sign;
+    char    *str;
 
     i = 0;
     sign = 0;
-    str = ft_calloc(13, sizeof(char));
+    str = (char *)malloc((ft_nblength(nb) + 1) * sizeof(char));
+    if (str == NULL)
+        return (NULL);
+    if (nb == 0)
+        return (str = "0\0");
     if (nb < 0)
     {
-        if (nb == -2147483648)
-        {
-          str = "-2147483648\0";
-          return (str);
-        }
+		if (nb == -2147483648)
+    		return (str = "-2147483648\0");
         nb *= --sign;
     }
     while (nb)
-    {
+	{
         str[i++] = (nb % 10) + '0';
-        nb /= 10;
-    }
+        nb/= 10;
+	}
     if (sign)
         str[i++] = '-';
     str[i] = '\0';
-    ft_rev_str(str);
+    return (ft_rev_str(str));
+}
+
+static int ft_nblength(int nb)
+{
+    int length;
+
+    length = 0;
+    if (nb <= 0)
+        length++;
+    while (nb)
+	{
+		nb /= 10;
+		length++;
+	}
+	return (length);
+}
+
+static char *ft_rev_str(char *str)
+{
+    int    i;
+    int    last;
+
+    i = 0;
+    last = ft_strlen(str) - 1;
+    while (i < last - i)
+    {
+        str[i]        = str[i] ^ str[last - i];
+        str[last - i] = str[i] ^ str[last - i];
+        str[i]        = str[i] ^ str[last - i];
+        i++;
+    }
     return (str);
 }
