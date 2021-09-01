0104e9eb222ba85fc8b9e9e28c28c8f044400564
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-01 20:19:55 -0300

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -1,7 +1,6 @@
 int ft_isalnum(int argument)
 {
-    if ((argument >= '0' && argument <= '9') || (argument >= 'a' && argument <= 'z') ||
-    (argument >= 'A' && argument >= 'Z'))
-        return (8);
-    return (0);
+    return ((argument >= '0' && argument <= '9') 
+        || (argument >= 'a' && argument <= 'z') 
+        || (argument >= 'A' && argument >= 'Z'));
 }
