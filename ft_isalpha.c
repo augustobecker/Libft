cb62bc90938839fca9ae60883a04040d639fa815
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-01 20:12:42 -0300

    Update ft_isalpha.c

--- ft_isalpha.c
+++ ft_isalpha.c
@@ -1,6 +1,4 @@
 int ft_isalpha(int argument)
 {
-	if ((argument >= 'a' && argument <= 'z') || (argument >= 'A' && argument <= 'Z'))
-		return (1024);
-	return (0);
+	return ((argument >= 'a' && argument <= 'z') || (argument >= 'A' && argument <= 'Z'));
 }
