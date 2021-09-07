03bfd271d38ed11361a4b8cfe42b55e3e9888d6a
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-07 10:10:00 -0300

    Update ft_lstlast.c

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -1,10 +1,7 @@
 #include "libft.h"
 t_list *ft_lstlast(t_list *lst)
 {
-        t_list 	*ptr_i;
-
-	 ptr_i = lst;
-	 while (ptr_i->next)
-		 ptr_i = ptr_i->next;
-	 return (ptr_i);
+	 while (lst->next)
+		 lst = lst->next;
+	 return (lst);
 }
