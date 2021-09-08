903532a977dcd9dcdd009425c8a68d8d30de87bc
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-08 10:19:27 -0300

    Update ft_isalnum.c

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,13 +6,12 @@
 /*   By: acesar-l <acesar-l@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 15:57:17 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/09/07 15:57:18 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/09/08 10:19:32 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 int	ft_isalnum(int argument)
 {
-	return ((argument >= '0' && argument <= '9')
-		|| (argument >= 'a' && argument <= 'z')
-		|| (argument >= 'A' && argument >= 'Z'));
+	return (ft_isdigit(argument) || ft_isalpha(argument));
 }
