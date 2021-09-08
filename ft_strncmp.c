34b018bbc1858f01cd187ec8152cafa562b99d0d
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-08 19:35:48 -0300

    Update ft_strncmp.c
    Update a condition while (i < size - 1)
    If you want to compare only 5 caracters, the comparison should stop at array[4], not array[5].

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -18,7 +18,7 @@ int	ft_strncmp(char *s1, char *s2, size_t size)
 	i = 0;
 	if (size == 0)
 		return (0);
-	while ((i < size) && (s1[i]) && (s1[i] == s2[i]))
+	while ((i < size - 1) && (s1[i]) && (s1[i] == s2[i]))
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
