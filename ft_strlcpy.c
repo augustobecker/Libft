a0c7c3b6a2c18efe8027b9920accc543421c77d4
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-10 14:05:35 -0300

    Update ft_strlcpy.c

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: acesar-l <acesar-l@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 16:34:03 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/09/10 02:25:42 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/09/10 14:05:42 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,8 @@ size_t	ft_strlcpy(char *dest, const char *src, size_t destsize)
 	size_t	i;
 
 	i = 0;
-	if (destsize < 0)
-		return (0);
+	if (destsize <= 0)
+		return (ft_strlen(src));
 	while (src[i] && i < destsize - 1)
 	{
 		dest[i] = src[i];
