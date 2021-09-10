97e6c3dc0e4f1eb542fcb7ff4f78fb57c4ede9c6
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-10 19:26:37 -0300

    Update ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,46 +6,44 @@
 /*   By: acesar-l <acesar-l@student.42sp.org>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 15:58:53 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/09/07 16:45:59 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/09/10 19:22:42 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-static	int	ft_nblength(int nb);
+static	int	ft_nblength(long int nb);
 
 static	char	*ft_rev_str(char *str);
 
 char	*ft_itoa(int nb)
 {
-	int		i;
-	int		sign;
-	char	*str;
+	int			i;
+	long int	sign;
+	long int	nbr;
+	char		*str;
 
 	i = 0;
 	sign = 0;
-	str = (char *)malloc((ft_nblength(nb) + 1) * sizeof(char));
+	nbr = (long int)nb;
+	str = (char *)ft_calloc((ft_nblength(nbr) + 1), sizeof(char));
 	if (str == NULL)
 		return (NULL);
-	if (nb == 0)
-		return (str = "0\0");
-	if (nb < 0)
+	if (nbr == 0)
+		str[i++] = '0';
+	if (nbr < 0)
+		nbr *= --sign;
+	while (nbr > 0)
 	{
-		if (nb == -2147483648)
-			return (str = "-2147483648\0");
-		nb *= --sign;
-	}
-	while (nb)
-	{
-		str[i++] = (nb % 10) + '0';
-		nb /= 10;
+		str[i++] = (nbr % 10) + '0';
+		nbr /= 10;
 	}
 	if (sign)
 		str[i++] = '-';
 	str[i] = '\0';
-	return (ft_rev_str(str));
+	return (str = ft_rev_str(str));
 }
 
-static	int	ft_nblength(int nb)
+static	int	ft_nblength(long int nb)
 {
 	int	length;
 
@@ -62,17 +60,17 @@ static	int	ft_nblength(int nb)
 
 static	char	*ft_rev_str(char *str)
 {
-	int	i;
-	int	last;
+	int	start;
+	int	final;
 
-	i = 0;
-	last = ft_strlen(str) - 1;
-	while (i < last - i)
+	start = 0;
+	final = ft_strlen(str) - 1;
+	while (start < final - start)
 	{
-		str[i] = str[i] ^ str[last - i];
-		str[last - i] = str[i] ^ str[last - i];
-		str[i] = str[i] ^ str[last - i];
-		i++;
+		str[start] = str[start] ^ str[final - start];
+		str[final - start] = str[start] ^ str[final - start];
+		str[start] = str[start] ^ str[final - start];
+		start++;
 	}
 	return (str);
 }
