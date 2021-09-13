faad2cab49cb9938d8e6b1f010f2222f51403522
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 18:55:10 -0300

    Update ft_calloc.c

--- ft_calloc.c
+++ ft_calloc.c
@@ -20,6 +20,6 @@ void	*ft_calloc(size_t nbr_elements, size_t element_size)
 	ptr = malloc(nbr_elements * element_size);
 	if (ptr == NULL)
 		return (NULL);
-	ft_bzero(ptr, element_size);
+	ft_bzero(ptr, nbr_elements * element_size);
 	return (ptr);
 }
