2ec869e585feeb05e216588774afa9b93b48377d
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 15:27:47 -0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strlcat.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acesar-l <acesar-l@student.42sp.org>       +#+  +:+       +#+        */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 16:26:39 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/09/07 17:05:42 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/09/13 15:27:42 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,21 @@
 size_t	ft_strlcat(char *dest, const char *src, size_t dest_size)
 {
 	size_t	i;
-	size_t	src_i;
+	size_t  dest_length;
+	size_t  src_length;
+	size_t  expected_dest_size;
 
 	i = 0;
-	src_i = ft_strlen(dest);
-	while (i < dest_size && src[i])
-		dest[src_i++] = src[i++];
-	dest[dest_size] = 0;
-	return (ft_strlen(dest));
+	dest_length = ft_strlen(dest);
+	src_length = ft_strlen(src);
+	if (dest_size > dest_length)
+	{
+	    expected_dest_size = dest_length + src_length;
+	    while (src[i] && dest_length < dest_size - 1)
+		    dest[dest_length++] = src[i++];
+	    dest[dest_length] = 0;
+	    return (expected_dest_size);
+	}
+	else
+	    return (src_length + dest_size);
 }
