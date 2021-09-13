1353f96a253046b5b3d2b0d9af185fe02e0bac1a
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 18:43:17 -0300

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -14,15 +14,15 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	size_t	i;
-	char	*p1;
-	char	*p2;
+	unsigned char	*p1;
+	unsigned char	*p2;
 
 	i = 0;
-	p1 = (char *)s1;
-	p2 = (char *)s2;
+	p1 = (unsigned char *) s1;
+	p2 = (unsigned char *) s2;
 	if (n == 0)
 		return (0);
 	while (i < n - 1 && p1[i] == p2[i])
 		i++;
-	return ((unsigned char)p1[i] - (unsigned char)p2[i]);
+	return (p1[i] - p2[i]);
 }
