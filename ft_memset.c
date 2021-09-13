e4aee9329e5d25fc73a332c48778bb5a4cf827cd
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 10:54:58 -0300

    Update ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -13,15 +13,13 @@
 #include "libft.h"
 void	*ft_memset(void *s, int argument, size_t length)
 {
-	size_t	i;
 	unsigned char	*str;
 
-	i = 0;
 	str = (unsigned char *)s;
-	while (i < length)
+	while (length > 0)
 	{
+		length--;
 		str[i] = (unsigned char) argument;
-		i++;
 	}
 	return (s);
 }
