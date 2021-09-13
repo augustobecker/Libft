54ad2c6caeb265b6abac9440ddeac5376d64b8bf
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 10:59:42 -0300

    Update ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -13,19 +13,17 @@
 #include "libft.h"
 void	*ft_memmove(void *dest, const void *src, size_t length)
 {
-	size_t		i;
 	char		*dest_ptr;
 	const char	*src_ptr;
 
-	i = length - 1;
 	dest_ptr = (char *) dest;
 	src_ptr = (const char *) src;
 	if (dest > src)
 	{
-		while (i - 1)
+		while (length > 0)
 		{
+			length--;
 			dest_ptr[i] = src_ptr[i];
-			i--;
 		}
 	}
 	else
