32da08fdebd265ef327baaeb691d51d8c794ced6
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 13:50:38 -0300

    Update ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -11,14 +11,14 @@
 /* ************************************************************************** */
 
 #include "libft.h"
-int	ft_strncmp(char *s1, char *s2, size_t size)
+int	ft_strncmp(const char *s1, const char *s2, size_t size)
 {
 	size_t	i;
 
 	i = 0;
 	if (size == 0)
 		return (0);
-	while ((i < size - 1) && (s1[i]) && (s1[i] == s2[i]))
+	while (i < size - 1 && s1[i] && s1[i] == s2[i])
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
