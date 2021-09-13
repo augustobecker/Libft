dfd015310ae2629777d4eedd3f414a58304ebb65
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 10:34:09 -0300

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -13,12 +13,5 @@
 #include "libft.h"
 void	ft_bzero(void *s, size_t length)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < length)
-	{
-		((char *)s)[i] = '\0';
-		i++;
-	}
+	ft_memset(s, '\0', length);
 }
