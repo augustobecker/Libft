726a9344027680e70a01cf0fdebe4c58afa572aa
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-13 14:10:05 -0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -3,33 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strnstr.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: acesar-l <acesar-l@student.42sp.org>       +#+  +:+       +#+        */
+/*   By: acesar-l <acesar-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/07 16:37:24 by acesar-l          #+#    #+#             */
-/*   Updated: 2021/09/07 17:09:25 by acesar-l         ###   ########.fr       */
+/*   Updated: 2021/09/13 14:09:42 by acesar-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-char	*ft_strnstr(char *str, char *to_find, size_t size)
+char	*ft_strnstr(const char *str, const char *to_find, size_t size)
 {
-	int	i;
-	int	find;
+	size_t	i;
+	size_t	find_size;
 
 	i = 0;
-	find = 0;
-	if (!to_find)
-		return (str);
-	while (str[i] && size - find)
+	find_size = ft_strlen(to_find);
+	if (!find_size)
+		return ((char *) str);
+	if (!size)
+		return (0);
+	while (str[i] && i + find_size <= size)
 	{
-		while (str[i + find] == to_find[find] && size - find)
-		{
-			if (!to_find[find + 1])
-				return (str + i);
-			find++;
-		}
+		if (!ft_strncmp(&str[i], to_find, find_size))
+			return ((char *) &str[i]);
 		i++;
-		size--;
 	}
 	return (0);
 }
