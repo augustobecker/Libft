c816ecd358dd18fec2406c17f81e56e6733b791b
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-14 01:04:47 -0300

    Update ft_lstadd_back.c

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -13,6 +13,8 @@
 #include "libft.h"
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	new->next = NULL;
-	ft_lstlast(*lst)->next = new;
+	if (!*lst)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
 }
