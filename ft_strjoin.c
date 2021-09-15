1ca842b1a3b6126ff3987430c0058a1ed828e7c5
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2021-09-15 20:32:45 -0300

    Security Update

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -14,7 +14,9 @@
 char	*ft_strjoin(const char *s1, const char *s2)
 {
 	char	*str;
-
+	
+	if (!str || !s2)
+		return (NULL);
 	str = (char *)ft_calloc((ft_strlen(s1) + ft_strlen(s2)) + 1, sizeof(char));
 	if (!str)
 		return (NULL);
