47bfa1c607ae89b4c7aaa66efd19951d81cbd77f
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-12 12:28:01 -0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 void	*ft_memcpy(void *dest, const void *src, size_t length)
 {
 	size_t		i;
