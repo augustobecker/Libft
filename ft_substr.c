99024a9b6b53c92512d7da1dcb443bb3d0acac44
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-12 12:23:53 -0300

    Update ft_substr.c

--- ft_substr.c
+++ ft_substr.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	substr_length;
