864d98925c9c484135160c1a71b0f8abf6124a30
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-12 12:26:04 -0300

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 size_t	ft_strlcat(char *dest, const char *src, size_t dest_size)
 {
 	size_t	i;
