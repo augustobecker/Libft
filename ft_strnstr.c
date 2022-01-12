dac2688a97d302e741c3ddf05cca8a485df87df6
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-12 12:24:49 -0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 char	*ft_strnstr(const char *str, const char *to_find, size_t size)
 {
 	size_t	i;
