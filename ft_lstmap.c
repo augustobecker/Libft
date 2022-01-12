8c5dbf565a64723eca3eaf645b00a2886c37d097
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-12 12:22:06 -0300

    Update ft_lstmap.c

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
 	t_list	*new_list;
