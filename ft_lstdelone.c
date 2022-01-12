dc695d0b9879a1b41085c0b096fa7c57df72823c
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-01-12 12:21:42 -0300

    Update ft_lstdelone.c

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -11,6 +11,7 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
 void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
 	(*del)(lst->content);
