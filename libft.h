d3ee42a7d248ec2d57b6c2518d98ac4e4b08bcde
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-09 14:33:46 -0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -70,7 +70,7 @@ void	ft_lstclear(t_list **lst, void (*del)(void*));
 void	ft_lstiter(t_list *lst, void (*f)(void *));
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *));
 
-int	ft_count_occurrences(char *str, char c);
+int	ft_count_occurrences(char **str, char c, int array_len);
 
 char	*get_next_line(int fd);
 
