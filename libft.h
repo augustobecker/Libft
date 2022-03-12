a879d075163e9baa7962cd425b269f2d0e0a44ac
Augusto Becker <81205527+augustobecker@users.noreply.github.com>
2022-03-12 13:46:57 -0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -82,7 +82,6 @@ int	ft_argument_percent(void);
 int	ft_argument_s(char *str);
 int	ft_argument_u(unsigned int nbr);
 int	ft_arguments_x(unsigned int nbr, char type);
-int	ft_is_argument(char a);
 int	ft_decimal_length(long int nbr);
 void	ft_free_ptr(char **ptr);
 char	ft_decimal_converter_to_hex(char digit, char type);
